3addb3f108122f5abcfcf1bcd38de70e21cfbcb0
Bao-chuan <bao65627@gmail.com>
2026-09-04 22:00:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-sb_publishable_7pNQIr21sL3EUy0WSqXKwQ_N6_oDWhJ
+const SUPABASE_URL="https://efpfhpwxmzpmmynczbce.supabase.co";
+const SUPABASE_ANON_KEY="sb_publishable_7pNQIr21sL3EUy0WSqXKwQ_N6_oDWhJ";
+const supabaseClient=window.supabase.createClient(SUPABASE_URL,SUPABASE_ANON_KEY);
+async function checkConnection(){
+ const {data}=await supabaseClient.auth.getSession();
+ console.log("Supabase connected",data);
+}
+checkConnection();
